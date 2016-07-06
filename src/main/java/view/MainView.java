2c7e3f7e30f8a9498cb4c187617ebd27e00f82c1
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-06 13:49:19 +0200

    [VIEW] Add an empty frame.
    This empty frame is contained into the main function.

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,10 +1,37 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by php on 05/07/16.
  */
 public class MainView {
+    /**
+     * The main frame in the whole project.
+     */
+    private static final JFrame frame = new JFrame();
+
+    /**
+     * An accessor on the main frame
+     *
+     * @return Returns the main frame
+     */
+    public static JFrame getFrame() {
+        return frame;
+    }
+
+    /**
+     * Entry point of the program
+     *
+     * @param args Args given to the program. Not used.
+     */
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            frame.setTitle("JukeBox");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.pack();
+        });
 
     }
 }
