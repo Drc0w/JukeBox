087b8ecf6172bfda51d19408e75c52c5cdaeb257
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-09 23:53:23 +0200

    [VIEW] Add class TabView.
    This will be used in order to paint the current Image which will later print
    the illustration of the album, and the playlist.

---
+++ src/main/java/view/TabView.java
@@ -0,0 +1,38 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by php on 09/07/16.
+ */
+public class TabView extends JPanel implements Observer {
+
+    private JSplitPane splitPane;
+
+    private ImageView imageView;
+
+    private PlayListView listView;
+
+    public TabView() {
+        super();
+        this.setLayout(new BorderLayout());
+
+        this.splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        this.splitPane.setOneTouchExpandable(true);
+
+        this.imageView = new ImageView();
+        this.listView = new PlayListView();
+
+        this.splitPane.setLeftComponent(this.imageView);
+        this.splitPane.setRightComponent(this.listView);
+        this.add(this.splitPane, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
+}
