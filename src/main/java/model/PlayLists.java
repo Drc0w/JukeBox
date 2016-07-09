872604e3d292952240edda06a47f8f294d6bd54c
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-09 18:24:44 +0200

    [MODEL] Add the singleton PlayLists.
    The project will include some tabs, every tab will be a different playlist. In
    order to handle all those playlists the class PlayLists has been added.

---
+++ src/main/java/model/PlayLists.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by php on 09/07/16.
+ */
+public class PlayLists extends Model {
+    private static final PlayLists PLAYLISTS = new PlayLists();
+
+    private ArrayList<PlayList> playLists;
+
+    private int currentPlaylist;
+
+    private PlayLists() {
+        this.playLists = new ArrayList<>();
+        this.playLists.add(new PlayList("P1"));
+        this.currentPlaylist = 0;
+    }
+
+    public static PlayLists getInstance() {
+        return PLAYLISTS;
+    }
+
+    public PlayList getCurrentPlayList() {
+        return this.playLists.get(this.currentPlaylist);
+    }
+
+    public void removePlayList(int position) {
+        this.playLists.remove(position);
+    }
+
+    public void addPlayList(PlayList playList) {
+        this.playLists.add(playList);
+    }
+
+    @Override
+    public void performAction(Object o) {
+
+    }
+}
