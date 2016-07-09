eabd5e03c2a104a48581898c5ac202d4a3a3e411
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-09 18:21:07 +0200

    [MODEL] Add the Song model.
    This class will be used in order to store a song as a model.
    This class is still a dummy class, it only contains accessor on the file and
    the path to the file. But it will later include the tag of the file.

---
+++ src/main/java/model/Song.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.io.File;
+
+/**
+ * Created by php on 09/07/16.
+ */
+public class Song extends Model {
+    /**
+     * The path to the file
+     */
+    private String path;
+
+    /**
+     * The file corresponding to the path.
+     * Null if the file does not exist.
+     */
+    private File file;
+
+    /**
+     * Default constructor of the class
+     *
+     * @param filename The path to the file that will be loaded
+     */
+    public Song(String filename) {
+        this.path = filename;
+        this.file = new File(filename);
+    }
+
+    public Song(File file) {
+        this.file = file;
+        this.path = file.getPath();
+    }
+
+    /**
+     * Accessor on the path
+     *
+     * @return Returns the path to the song
+     */
+    public String getPath() {
+        return this.path;
+    }
+
+    /**
+     * Accessor on the file
+     *
+     * @return Returns the file corresponding to the path
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /**
+     * This function will update the model and notify the observers
+     *
+     * @param o Object used in order to update the model
+     */
+    @Override
+    public void performAction(Object o) {
+
+    }
+}
