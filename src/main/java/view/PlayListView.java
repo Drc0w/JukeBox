764b7c5fe68a979b3f26c17a7229a35097fe17f0
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-10 01:31:51 +0200

    [VIEW] Implementation of function update in PlayListView.
    The class is now updated from the observables. The function reloads the list
    with the name of the songs contained in the corresponding playlist.

--- src/main/java/view/PlayListView.java
+++ src/main/java/view/PlayListView.java
@@ -1,7 +1,10 @@
 package view;
 
+import utils.Song;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -9,7 +12,7 @@ import java.util.Observer;
  * Created by php on 09/07/16.
  */
 public class PlayListView extends JPanel implements Observer {
-    private JList jList;
+    private JList<String> jList;
 
     private DefaultListModel<String> listModel;
 
@@ -21,7 +24,7 @@ public class PlayListView extends JPanel implements Observer {
 
         this.jList = new JList<>(this.listModel);
         this.jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        this.jList.setPreferredSize(new Dimension(200, 600));
+        this.jList.setPreferredSize(new Dimension(100, 100));
 
         this.scrollPane = new JScrollPane(this.jList);
 
@@ -30,6 +33,19 @@ public class PlayListView extends JPanel implements Observer {
 
     @Override
     public void update(Observable observable, Object o) {
+        ArrayList<Song> songs = (ArrayList<Song>) o;
 
+        this.remove(this.scrollPane);
+
+        this.listModel = new DefaultListModel<>();
+        for (Song song : songs) {
+            this.listModel.addElement(song.getFile().getName());
+        }
+        this.jList = new JList<>(this.listModel);
+        this.scrollPane = new JScrollPane(this.jList);
+        this.add(this.scrollPane);
+        
+        this.repaint();
+        this.revalidate();
     }
 }
