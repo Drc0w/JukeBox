cbfa6478f4700c5e75b6b061632390ee6e2ab818
Paul-Henry Perrissel <perris_p@epita.fr>
2016-07-11 00:18:24 +0200

    [CONTROLLER] Update DeleteTabController.
    If the desired tab to close is the current playlist, it will stop playling.

--- src/main/java/controller/DeleteTabController.java
+++ src/main/java/controller/DeleteTabController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.PlayLists;
+import player.MusicPlayerService;
 import view.TabsView;
 
 import java.awt.event.ActionEvent;
@@ -14,6 +15,9 @@ public class DeleteTabController implements ActionListener {
     public void actionPerformed(ActionEvent actionEvent) {
         if (TabsView.getTabsView().getTabCount() != 0) {
             int pos = TabsView.getTabsView().getSelectedIndex();
+            if (pos == PlayLists.getInstance().getCurrentIndex()) {
+                MusicPlayerService.getInstance().stop();
+            }
             TabsView.getTabsView().remove(pos);
             PlayLists.getInstance().removePlayList(pos);
         }
